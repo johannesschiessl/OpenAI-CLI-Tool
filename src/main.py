c9af96c58133088eaaf3ea87af14ef676aa4d235
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2023-12-27 13:49:09 +0100

    Change module call after Restructure imports

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from cli.interface import *
 from cli.commands import *
 
 def main():
-    user_name = cli.interface.initiliaze(config)
+    user_name = initiliaze(config)
 
     while True:
-        prompt = cli.interface.user_input(user_name)
+        prompt = user_input(user_name)
 
         if cli.commands.check_commands(prompt, user_name) is False:
             cli.interface.assistant_output(openai_handler.generate_text(user_name, prompt))
