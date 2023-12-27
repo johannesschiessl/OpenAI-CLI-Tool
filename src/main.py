9aeb126493bf8af991059192235a03267c16f8cd
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2023-12-27 13:26:28 +0100

    Add cli

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 import config
-import openai_handler
+import openai_handler.text_generation as openai_handler
+import cli.interface, cli.commands
 
 def main():
-    pass
+    user_name = cli.interface.initiliaze(config)
+
+    while True:
+        prompt = cli.interface.user_input(user_name)
+
+        if cli.commands.check_commands(prompt, user_name) is False:
+            cli.interface.assistant_output(openai_handler.generate_text(user_name, prompt))
 
 if __name__ == '__main__':
     main()
