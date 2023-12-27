44bea2f609f7ef8447d50662660ce3cb2fd90de7
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2023-12-27 13:26:46 +0100

    Add cli interface.py

---
+++ src/cli/interface.py
@@ -0,0 +1,24 @@
+class styles:
+    BOLD_START = "\033[1m"
+    BOLD_END = "\033[0m"
+
+    FONT_SIZE_START = "\033[6;60;]"
+    FONT_SIZE_END = "\033[0m"
+
+
+def initiliaze(config):
+    print(f"{styles.BOLD_START}{styles.FONT_SIZE_START}🧠 AI-Assistant (v{config.VERSION}){styles.FONT_SIZE_END}{styles.BOLD_END}")
+
+    print("\n🚀 Type '/quit' to exit the program.")
+    print("ℹ️ Type '/help' to see the list of commands.")
+
+    user_name = input("\n📝 Type in your name: ")
+
+    print(f"\n🧠 Assistant: 👋 Hi, {user_name}! How can I help you today?")
+    return user_name
+
+def user_input(user_name):
+    return input(f"😀 {user_name}: ")
+
+def assistant_output(response):
+    print(f"🧠 Assistant: {response}")
