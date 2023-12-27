e377b5132a4362faba014e03079e514042d473a2
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2023-12-27 13:48:01 +0100

    Add generate_image()

---
+++ src/openai_handler/image_generation.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+
+def generate_image(prompt):
+    client = OpenAI()
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    return response.data[0].url
