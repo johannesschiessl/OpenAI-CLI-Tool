f5c245c9bd0b44b8aa9c182cf80c0802eced7a7f
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2023-12-27 11:47:19 +0100

    Add generate_text()

--- src/openai_handler/text_generation.py
+++ src/openai_handler/text_generation.py
@@ -3,3 +3,15 @@ import datetime
 
 def get_current_date():
     return datetime.datetime.now().strftime("%Y-%m-%d")
+
+def generate_text(user_name, prompt):
+    client = OpenAI()
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": f"You are a helpful assistant, a large language model trained by OpenAI, based on the GPT-3.5 architecture. Current User's name: {user_name} - Knowledge cutoff: 2022-01 - Current date: {get_current_date()}"},
+            {"role": "user", "content": prompt},
+        ]
+    )
+
+    return response.choices[0].message.content
