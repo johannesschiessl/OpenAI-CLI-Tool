13e420b700593b9cf7087491b7fe8fde40d1c5bc
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2023-12-28 18:28:19 +0100

    Add option to choose model

--- src/openai_handler/text_generation.py
+++ src/openai_handler/text_generation.py
@@ -4,10 +4,10 @@ import datetime
 def get_current_date():
     return datetime.datetime.now().strftime("%Y-%m-%d")
 
-def generate_text(user_name, prompt):
+def generate_text(user_name, model, prompt):
     client = OpenAI()
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model=model,
         messages=[
             {"role": "system", "content": f"You are a helpful assistant, a large language model trained by OpenAI, based on the GPT-3.5 architecture. Answer briefly and accurately. Current User's name: {user_name} - Knowledge cutoff: 2022-01 - Current date: {get_current_date()}"},
             {"role": "user", "content": prompt},
