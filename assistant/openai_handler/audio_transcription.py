a9af0e043de87592d9e45b17eac94aa9641f9eec
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-01-14 18:15:52 +0100

    Add error handlers

--- assistant/openai_handler/audio_transcription.py
+++ assistant/openai_handler/audio_transcription.py
@@ -6,11 +6,23 @@ def write_output_to_file(output, file_path):
 
 def transcribe_audio(file_path):
     client = OpenAI()
-    response = client.audio.transcriptions.create(
-        model="whisper-1",
-        file=open(file_path, "rb"),
-        response_format="text",
-    )
+
+    try:
+        file = open(file_path, "rb")
+    except:
+        print("❌ Error: File not found. Please provide a valid file path.")
+        return
+
+    try:
+        response = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=file,
+            response_format="text",
+        )
+    except:
+        print("❌ Error: An error occurred while transcribing the audio. Please try again.")
+        return
+
 
     write_output_to_file(response, "data\\ai_assistant_transcription.txt")
     return response
