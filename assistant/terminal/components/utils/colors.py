06b442395468b3169609f563a32b6674bb168d34
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-01-15 20:02:00 +0100

    Add colors.py

---
+++ assistant/terminal/components/utils/colors.py
@@ -0,0 +1,17 @@
+def red(text):
+    return "\033[91m{}\033[0m".format(text)
+
+def green(text):
+    return "\033[92m{}\033[0m".format(text)
+
+def yellow(text):
+    return "\033[93m{}\033[0m".format(text)
+
+def blue(text):
+    return "\033[94m{}\033[0m".format(text)
+
+def purple(text):
+    return "\033[95m{}\033[0m".format(text)
+
+def cyan(text):
+    return "\033[96m{}\033[0m".format(text)
