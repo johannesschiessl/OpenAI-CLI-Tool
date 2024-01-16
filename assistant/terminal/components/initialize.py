74066514799f74d8c5a1591cda9f37f8f79e7357
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-01-16 06:03:25 +0100

    Add help information

--- assistant/terminal/components/initialize.py
+++ assistant/terminal/components/initialize.py
@@ -2,7 +2,7 @@ import json
 
 def initialize():
 
-    from utils.colors import green, red
+    from utils.colors import green, red, cyan, purple
     from utils.find_path_to_data_file import find_path_to_data_file
 
     USERNAME_FILE_PATH = find_path_to_data_file("config.json")
@@ -13,4 +13,9 @@ def initialize():
         gpt_model = config_data.get("gpt_model", "")
 
     print(green(f"AI-Assistant ({version})"))
-    print(red(f"\nModel: {gpt_model}\n"))
+    print(red(f"\nModel: {purple(gpt_model)}\n"))
+
+    print(f"Type {cyan('/help')} for a list of commands.")
+    print(f"Type {cyan('/exit')} to exit the program.\n")
+
+    print(f"Type {cyan('/reset')} to reset the conversation history.\n")
