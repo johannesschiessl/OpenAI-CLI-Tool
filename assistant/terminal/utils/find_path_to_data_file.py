3c80dbb809c14e09f51e44f33ccca3d38f9679f1
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-01-17 13:51:12 +0100

    Fix wrong filepath

--- assistant/terminal/utils/find_path_to_data_file.py
+++ assistant/terminal/utils/find_path_to_data_file.py
@@ -3,7 +3,7 @@ import os
 def find_path_to_data_file(file):
 
     current_script_path = os.path.abspath(__file__)
-    ai_assistant_dir = os.path.abspath(os.path.join(current_script_path, '..', '..', '..', '..', '..', '..'))
+    ai_assistant_dir = os.path.abspath(os.path.join(current_script_path, '..', '..', '..', '..', '..'))
     file_path = os.path.join(ai_assistant_dir, 'data', file)
 
     return file_path
