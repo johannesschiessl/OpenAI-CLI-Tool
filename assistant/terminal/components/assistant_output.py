0f4ccdf3046dacb2a84ff4ae0c579dc55ff72a5b
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-01-17 14:58:46 +0100

    Move code_block.py to assistant_output.py

--- assistant/terminal/components/assistant_output.py
+++ assistant/terminal/components/assistant_output.py
@@ -1,4 +1,21 @@
-def assistant_output(text):
-    from terminal.utils.colors import light_gray
+from rich.console import Console
+from rich.syntax import Syntax
+from rich.panel import Panel
+from rich.style import Style
 
-    print(light_gray("\n  " + text + "\n"))
+from terminal.utils.colors import light_gray
+
+def print_assistant_output(output):
+    print(light_gray(f"\n{output}"))
+
+
+def print_code_block(code, language='python'):
+    syntax = Syntax(code, language, theme="monokai", line_numbers=True)
+
+    custom_style = Style(bgcolor="dark_sea_green")
+
+    panel = Panel(syntax, style=custom_style, border_style="blue")
+
+    console = Console()
+    console.print(panel)
+
