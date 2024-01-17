f8d0937d271fa0062ff5b6686735a4f18d4bed75
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-01-17 15:13:13 +0100

    Remove unused code

--- assistant/terminal/terminal.py
+++
@@ -1,9 +0,0 @@
-from terminal.components.initialize import *
-from terminal.components.user_input import *
-from terminal.components.assistant_output import *
-
-
-def terminal():
-    initialize()
-
-terminal()
