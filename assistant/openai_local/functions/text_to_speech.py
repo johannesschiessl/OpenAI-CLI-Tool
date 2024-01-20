13977d19b41102bc895b5fb15d610ae48b9894ec
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-01-20 08:34:18 +0100

    Fix wrong openai module

--- assistant/openai_local/functions/text_to_speech.py
+++ assistant/openai_local/functions/text_to_speech.py
@@ -1,18 +1,25 @@
-import openai
+from openai import OpenAI
 from utils.date_time_handling import *
+from utils.file_handling import *
 from terminal.components.system_messages import *
+from terminal.components.assistant_output import print_text_to_speech
 
 
 def text_to_speech(prompt):
     try:
-        response = openai.Audio.create(
-            model="whisper-1",
-            file=open(prompt, "rb"),
+        client = OpenAI()
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input=prompt,
         )
-    except:
+
+    except Exception as e:
+        print(e)
         error_openai()
         return None
 
 
-    response.stream_to_file("data\\ai_assistant_tts.mp3")
-    return "data\\ai_assistant_tts.mp3"
+    response.stream_to_file(find_path_to_data_file("tts.mp3"))
+
+    print_text_to_speech(find_path_to_data_file("tts.mp3"))
