6ac067ea047c393938cef0c389715f88d4615177
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-08-16 23:25:48 +0200

    Update available models

--- assistant/terminal/components/system_messages.py
+++ assistant/terminal/components/system_messages.py
@@ -29,10 +29,8 @@ def message_configure():
     Function to configure and change the current model for the application.
     """
     print(f"\n{red('Available Models:')}")
-    print(f"#1     {purple('gpt-3.5-turbo')}")
-    print(f"#2     {purple('gpt-4-1106-preview')}")
-    print(f"#3     {purple('gpt-4')}")
-    print(f"#4     {purple('gpt-4-32k')}")
+    print(f"#1     {purple('gpt-4o')}")
+    print(f"#2     {purple('gpt-4o-mini')}")
 
     with open(find_path_to_data_file("config.json"), "r") as file:
         model_data: dict = json.load(file)
@@ -45,13 +43,9 @@ def message_configure():
     model_user_input: str = input("#")
 
     if model_user_input == "1":
-        new_model:str = "gpt-3.5-turbo"
+        new_model:str = "gpt-4o"
     elif model_user_input == "2":
-        new_model:str = "gpt-4-turbo-preview"
-    elif model_user_input == "3":
-        new_model:str = "gpt-4"
-    elif model_user_input == "4":
-        new_model:str = "gpt-4-32k"
+        new_model:str = "gpt-4o-mini"
     else:
         print(red("\nInvalid model. Set to previous model."))
         new_model: str = current_model
